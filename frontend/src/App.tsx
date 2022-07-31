1d0b0151b30a3f8323cd9803717cbec7a5fb98b6
Emilene Goulart <emigoulart@gmail.com>
2022-07-31 14:47:28 -0300

    Added css styles

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './assets/styles/custom.scss'
+import './App.css';
+import Navbar from 'components/Navbar';
+
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <h1>Conheça o melhor catálogo de produtos</h1>
+    </>
+  );
+}
+
+export default App;

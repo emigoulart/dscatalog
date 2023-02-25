349ac75e619c22409926d4719a3791028336462e
emigoulart <emigoulart@gmail.com>
2023-02-24 22:48:08 -0300

    Pagination style

--- frontend/src/pages/Catalog/index.tsx
+++ frontend/src/pages/Catalog/index.tsx
@@ -1,3 +1,4 @@
+import Pagination from 'components/Pagination';
 import ProductCard from 'components/ProductCard';
 import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
@@ -22,8 +23,9 @@ const Catalog = () => {
     ],
   };
   return (
-    <div className="container my-4">
-      <div className="row">
+    <div className="container my-4 catalog-container">
+       <div className="row catalog-title-container">
+        <h1>Catálogo de produtos</h1>
         <div className="col-sm-6 col-lg-4 col-xl-3">
           <Link to="/products/1">
             <ProductCard product={product} />
@@ -49,6 +51,9 @@ const Catalog = () => {
             <ProductCard product={product} />
           </Link>
         </div>
+      </div>
+      <div className='row'>
+        <Pagination/>
       </div>
     </div>
   );
